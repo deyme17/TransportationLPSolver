dff79e50545df95eef18bbbfc27d4c07792f394f
dmytro <dmytro19022006@gmail.com>
2025-11-15 15:17:58 +0200

    0

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,7 @@
-from .constants import (
-    AppConstants, 
-    InputWidgetConstants, OptimizationType, ConstraintOperator, 
-    ResultConstants, SolutionStatus, StatusColor
-)
-from .containers import ConstraintData, LPProblem, LPResult, BFSolution
+from .constants import AppConstants
+from .containers import TLPProblem, TLPResult, BFSolution
 from .formatters import ResultFormatter
 from .stylesheet import StyleSheet
-from .ui_helper import UIHelper, ResultUIHelper, SimplexTableManager
+from .ui_helper import UIHelper
 from .validators import InputValidator
-from .interfaces import ITable, IBFSFinder, ISimplexAlgorithm
+from .interfaces import IBFSFinder, ITransportationAlgorithm
--- utils/formatters.py
+++ utils/formatters.py
@@ -1,5 +1,4 @@
 from typing import List, Optional
-from utils.constants import ResultConstants
 
 class ResultFormatter:
     """Formats optimization results for display"""
